1f4398fd1b720ce05d07e32b3d66d27fbaf1c490
Donald Sharp <sharpd@nvidia.com>
2024-01-25 12:53:24 -0500

    bgpd: backpressure - Add a typesafe list for Zebra Announcement
    Modify the bgp master to hold a type safe list for bgp_dests that need
    to be passed to zebra.
    
    Future commits will use this.
    
    Ticket: #3390099
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>
    Signed-off-by: Rajasekar Raja <rajasekarr@nvidia.com>

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -18,6 +18,8 @@
 #include "iana_afi.h"
 #include "asn.h"
 
+PREDECL_LIST(zebra_announce);
+
 /* For union sockunion.  */
 #include "queue.h"
 #include "sockunion.h"
@@ -175,6 +177,9 @@ struct bgp_master {
 	struct event *t_bgp_start_label_manager;
 
 	bool v6_with_v4_nexthops;
+
+	/* To preserve ordering of installations into zebra across all Vrfs */
+	struct zebra_announce_head zebra_announce_head;
 
 	QOBJ_FIELDS;
 };
